2ecd24a269ff61f00566779bdf081f886dd79b05
Akintoye Peterpaul <appeimisic@gmail.com>
2026-01-27 14:26:54 +0100

    cors issue fixed?

--- internal/middlewares/cors_middleware.go
+++ internal/middlewares/cors_middleware.go
@@ -10,10 +10,7 @@ func CORSMiddleware() *cors.Cors {
 	return cors.New(cors.Options{
 		// AllowedOrigins:   []string{"http://localhost:3000", "http://localhost:5173"},
 		AllowOriginFunc: func(origin string) bool {
-			if origin == "https://deployed-frontend.com" {
-			return true
-			}
-			return strings.HasPrefix(origin, "http://localhost:")
+			return strings.HasPrefix(origin, "http://localhost:") || origin == "http://deployment.com"
 		},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Authorization", "Content-Type", "X-Requested-With", "Application"},
